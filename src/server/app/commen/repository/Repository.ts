105a7884130a30f601afc65b9a172d7096d977e4
daniel glenn <linuxbender@gmail.com>
2016-01-13 01:21:25 +0100

    remove angular legacy tsd

--- src/server/app/commen/repository/Repository.ts
+++ src/server/app/commen/repository/Repository.ts
@@ -4,8 +4,42 @@ namespace theforce.server.app.commen.repository {
     /**
      * Repository
      */
-    export class Repository implements IRepository<string>{
+    
+    export class Repository implements IRepository<Person> {
+        
         constructor() {
         }
+        
+        GetById(Id:Number): Promise<Person[]> {
+            return null;
+        }
+        
+        First(func:(Person)): Promise<Person> {
+            return null;
+        }
+        
+        Find(func:(Person)):Promise<Person[]> {
+            return null;
+        }
+         
+        GetAll():Promise<Person[]>{
+            return null;
+        }
+        
+        Update(entity:Person): Promise<void>{
+            return null;
+        }
+        
+        Insert(entity:Person): Promise<void>{
+            return null;
+        }
+        
+        Delete(entity:Person): Promise<void>{
+            return null;
+        }
+    }
+    
+    export class Person {
+        
     }
 }
